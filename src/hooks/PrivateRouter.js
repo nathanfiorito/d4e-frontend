b33b05206e5cb74d49e1e307562ed1abb4823ba5
Nathan Fiorito <nfiorito64@gmail.com>
2021-06-01 13:53:33 -0300

    Refatorado metódos de autenticação

--- src/hooks/PrivateRouter.js
+++ src/hooks/PrivateRouter.js
@@ -1,13 +1,11 @@
-import {Route, Redirect } from 'react-router-dom'
-import Auth from '../services/auth'
-
-const auth = new Auth();
+import { Route, Redirect } from 'react-router-dom'
+import { isAuthenticated } from '../services/auth'
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
       {...rest}
       render={props =>
-        auth.isAuthenticated() ? (
+        isAuthenticated() ? (
           <Component {...props} />
         ) : (
           <Redirect to={{ pathname: "/signin", state: { from: props.location } }} />
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,17 +1,39 @@
 import {api} from './api';
+import jwt from 'jsonwebtoken';
 
-export default class Auth{
-  signIn(username, password){
-    api.post('auth/signin',{
-      username: username,
-      password: password
-    }).then((res) => {
-      localStorage.setItem('accessToken', res.data.accessToken)
-    })
+function signIn(username, password){
+  if(isAuthenticated()) return;
+  api.post('auth/signin',{
+    username: username,
+    password: password
+  }).then((res) => {
+    localStorage.setItem('accessToken', res.data.accessToken)
+  })
+}
+
+function getAccessToken() {
+  return localStorage.getItem('accessToken');
+}
+
+function removeAccessToken() {
+  localStorage.removeItem('accessToken');
+}
+
+function isAuthenticated (){
+  const token = getAccessToken();
+  if(!token){
+    return false;
   }
+   
+  var decodedToken = jwt.decode(token, {complete: true});
+  var dateNow = new Date();
 
-  isAuthenticated (){
-    if(localStorage.getItem('accessToken')) return true;
-    else return false;
+  if(decodedToken.payload.exp < dateNow.getTime()){
+    return true;
+  } else {
+    removeAccessToken();
+    return false;
   }
 }
+
+export {signIn, getAccessToken, removeAccessToken, isAuthenticated}
