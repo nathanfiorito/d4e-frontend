5f64997dc01dd4bc38c3cd7d2df2057c19e2222b
Nathan Fiorito <nfiorito64@gmail.com>
2021-06-01 13:52:36 -0300

    Adicionado Links em modo de SPA

--- src/components/SurveyHeader/SurveyHeader.js
+++ src/components/SurveyHeader/SurveyHeader.js
@@ -1,14 +1,18 @@
-import {Nav} from 'react-bootstrap'
-import { propTypes } from 'react-bootstrap/esm/Image'
+import {Nav} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function SurveyHeader(props) {
     return (
         <Nav as="ul">
         <Nav.Item as="li">
-          <Nav.Link href="/surveys">Listar pesquisas</Nav.Link>
+          <LinkContainer to="/surveys">
+            <Nav.Link>Listar pesquisas</Nav.Link>
+          </LinkContainer>
         </Nav.Item>
         <Nav.Item as="li">
-          <Nav.Link href="/create/surveys">Cadastrar</Nav.Link>
+          <LinkContainer to="/create/surveys">
+            <Nav.Link>Cadastrar</Nav.Link>
+          </LinkContainer>
         </Nav.Item>
       </Nav>
     );
