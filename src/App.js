ac851c39a57b9ff36417969ad8163fec075bf6b6
Nathan Fiorito <nfiorito64@gmail.com>
2021-06-01 13:51:44 -0300

    Criado React Hook para verificar status de login

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ import CreateSurvey from './pages/CreateSurvey/CreateSurvey';
 import Survey from './pages/Survey/Survey';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthRoute from './hooks/AuthRoute';
 
 export default function App(){
     return(
         <BrowserRouter>
         <Switch>
             <Route path='/' exact={true} component={Home}/>
-            <Route path='/signin' exact={true} component={Login}/>
-            <Route path='/signup' exact={true} component={SignUp}/>
+            <AuthRoute path='/signin' exact={true} component={Login}/>
+            <AuthRoute path='/signup' exact={true} component={SignUp}/>
             <PrivateRoute path='/surveys' exact={true} component={SurveyList}/>
             <PrivateRoute path='/surveys/:id' exact={true} component={Survey}/>
             <PrivateRoute path='/create/surveys' exact={true} component={CreateSurvey}/>
